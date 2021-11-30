279fe36bf9e73c81fd745f43e8aea5995721bba7
OlenaKulchytska <olenakulchutska3@gmail.com>
2021-11-30 17:30:08 +0200

    correct

--- OpenCart/src/main/java/com/opencart/repository/AppleCinemaModelRepository.java
+++ OpenCart/src/main/java/com/opencart/repository/AppleCinemaModelRepository.java
@@ -3,11 +3,23 @@ package com.opencart.repository;
 import com.opencart.datamodel.AppleCinemaModel;
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
 public class AppleCinemaModelRepository {
 
     private AppleCinemaModelRepository() {}
 
     public static AppleCinemaModel getAppleCinemaModel() {
+        LocalTime time = LocalTime.now();
+        LocalDate date = LocalDate.now();
+        LocalDateTime dateTime = LocalDateTime.now();
         return AppleCinemaModel.builder()
                 .radio(7) // 5/6/7
                 .checkbox(11) // 8/9/10/11
@@ -15,10 +27,10 @@ public class AppleCinemaModelRepository {
                 .select(1) // 1/2/3/4
                 .textarea(RandomStringUtils.randomAlphabetic(20))
                 .file("src/test/additionalFiles/File.txt")
-                .date("2021-02-20")
-                .time("00:00")
-                .dateAndTime("2021-11-10 00:00")
-                .qty("3")
+                .date(date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+                .time(time.format(DateTimeFormatter.ofPattern("HH:mm")))
+                .dateAndTime(dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
+                .qty(RandomStringUtils.randomNumeric(2))
                 .build();
     }
 }
--- OpenCart/src/main/java/com/opencart/repository/CanonEOS5DModelRepository.java
+++ OpenCart/src/main/java/com/opencart/repository/CanonEOS5DModelRepository.java
@@ -1,6 +1,7 @@
 package com.opencart.repository;
 
 import com.opencart.datamodel.CanonEOS5DModel;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class CanonEOS5DModelRepository {
 
@@ -9,7 +10,7 @@ public class CanonEOS5DModelRepository {
     public static CanonEOS5DModel getCanonEOS5DModel() {
         return CanonEOS5DModel.builder()
                 .select(15) // 15/16
-                .qty("1")
+                .qty(RandomStringUtils.randomNumeric(1,2))
                 .build();
     }
 }
